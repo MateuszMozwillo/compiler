b181bd754771dac84376f46730634f9ab312098a
MateuszMozwillo <109803607+MateuszMozwillo@users.noreply.github.com>
2025-08-11 01:16:20 +0200

    changes

--- src/main.c
+++ src/main.c
@@ -5,16 +5,6 @@ int main() {
     char* to_tokenize = "var i = 10; function asd(v1, v2) {return v1 + v2; }";
     TokenVec tokenized = tokenize(to_tokenize, strlen(to_tokenize));
 
-    // printf("\n");
-    // for (size_t j = 0; j < 100000000; j++) {
-    //     TokenVec tokenized = tokenize(to_tokenize, strlen(to_tokenize));
-    //     token_vec_cleanup(tokenized);
-    //     if (j % 10000 == 0) {
-    //         printf("%lld\n", j);
-    //     }
-        
-    // }
-
     for (size_t i = 0; i < tokenized.len; i++) {
         printf(" %s" , tokenized.ptr[i].content);
     }
